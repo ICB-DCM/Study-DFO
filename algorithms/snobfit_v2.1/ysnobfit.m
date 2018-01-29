100e2fe8e8d6ef9a4585eacb4eab866247ed1b20
yannikschaelte <yannik.schaelte@helmholtz-muenchen.de>
2018-01-29 22:42:08 +0100

    minor error ysnobfit

--- algorithms/snobfit_v2.1/ysnobfit.m
+++ algorithms/snobfit_v2.1/ysnobfit.m
@@ -9,7 +9,7 @@ n = size(u,1);
 npoint = n+6; % number of random start points to be generated
 nreq = n+6; % number of points to be generated in each call to snobfit
 x = rand(npoint,n);
-f = zeros(npoint,1);
+f = zeros(npoint,2);
 x = x*diag(v-u) + ones(npoint,1)*u'; % generation of npoint random starting points in [u,v]
 dx = (v-u)'*1e-5; % resolution vector
 p = 0.5; % probability of generating a point of 'class 4'
@@ -17,7 +17,7 @@ params = struct('bounds',{u,v},'nreq',nreq,'p',p);
 
 % compute function values
 for j=1:npoint
-    f(j) = fun(x(j,:));
+    f(j,:) = [fun(x(j,:)), -1];
 end
 funEvals = npoint;
 [fbest,jbest] = min(f);
@@ -36,10 +36,10 @@ while funEvals < options.MaxFunEvals
     clear x
     clear f
     x = rand(nreq,n);
-    f = zeros(nreq,n);
+    f = zeros(nreq,2);
     for j=1:nreq % evaluate f at new proposed points
         x(j,:) = request(j,1:n);
-        f(j) = objfun(x(j,:));
+        f(j,:) = [objfun(x(j,:)), -1];
     end
     funEvals = funEvals + nreq;
     [fbestnew,jbestnew] = min(f);
