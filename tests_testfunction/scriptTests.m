e3b25dd20cc1912b34e2cd5ab2a1b2f907e02d39
yannikschaelte <yannik.schaelte@gmail.com>
2018-02-03 17:34:42 +0100

    error correct scriptTests

--- tests_testfunction/scriptTests.m
+++ tests_testfunction/scriptTests.m
@@ -1,14 +1,14 @@
 solvers_local = C.cell_solvers_local;
 for j = 1:length(solvers_local)
 	if ~ismember(solvers_local{j},{'gps','gss','mads'})
-		runTests(solvers_local{j},2000,20,false);
-		clear;
+		res = runTests(solvers_local{j},2000,20,false);
+		clear res;
 	end
 end
 solvers_global = C.cell_solvers_global;
 for j = 1:length(solvers_global)
 	if ~ismember(solvers_global{j},{'ga','particleswarm'})
-		runTests(solvers_global{j},2000,20,false);
-		clear;
+		res = runTests(solvers_global{j},2000,20,false);
+		clear res;
 	end
 end
