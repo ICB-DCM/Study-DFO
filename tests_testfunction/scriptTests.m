056607ffdb91c781b271c53b3e2ba4f544d4df79
yannikschaelte <yannik.schaelte@gmail.com>
2018-02-03 17:32:29 +0100

    m_global solvers: other handling

--- tests_testfunction/scriptTests.m
+++ tests_testfunction/scriptTests.m
@@ -1,9 +1,14 @@
 solvers_local = C.cell_solvers_local;
 for j = 1:length(solvers_local)
-    runTests(solvers_local{j},2000,20,false);
-    clear;
+	if ~ismember(solvers_local{j},{'gps','gss','mads'})
+		runTests(solvers_local{j},2000,20,false);
+		clear;
+	end
 end
 solvers_global = C.cell_solvers_global;
 for j = 1:length(solvers_global)
-    runTests(solvers_global{j},2000,20,false);
+	if ~ismember(solvers_global{j},{'ga','particleswarm'})
+		runTests(solvers_global{j},2000,20,false);
+		clear;
+	end
 end
