eb34cf19210716a362b4decc3d8c76bfba021aa3
yannikschaelte <yannik.schaelte@gmail.com>
2018-07-27 23:27:17 +0200

    error correct tests_testfunction/totalstats

--- tests_testfunction/totalstats.m
+++ tests_testfunction/totalstats.m
@@ -1,10 +1,15 @@
-files = dir('results/cell_results_*.mat')
+%cd results
+
+files = dir('results/cell_results_test_*');
 
 sum_time = 0;
 
-for file = files
-    load(file.name);
+for jFile = 1:length(files)
+    file = files(jFile);
+    load(['results/' file.name]);
     for jResult = 1:length(cell_results)
         sum_time = sum_time + cell_results{jResult}.time;
     end
 end
+
+disp(['total time: ' num2str(sum_time) 's = ' num2str(sum_time/3600) 'h']);
